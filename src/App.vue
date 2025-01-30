2f622a9ce9de9cc4b9876281b775e8016a57aa8f
Fobiak <saysanov2018@mail.ru>
2025-01-31 02:48:19 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="css" src="./app.css"></style>
+<script setup lang="ts">
+</script>
---
+++ src/components/header/header.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+   <div class="header">
+     <h1 class="header__title">ReadRelay</h1>
+     <div class="header__navigation"></div>
+        <div class="header__navigation-list">
+          <router-link to="/" class="header-btn">Главная</router-link>
+          <router-link to="/about" class="header-btn">О нас</router-link>
+        </div>
+
+
+   </div>
+</template>
+
+
+
+<style lang="css" src="header.css"></style>
